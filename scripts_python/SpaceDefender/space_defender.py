e6ad76cbbe598e27d53b775d31c82f12ae0af39d
RobinN7 <robin.beilvert@gmail.com>
2014-12-25 16:48:31 +0100

    Ajout de tirs bonus sur SpaceDefender

--- scripts_python/SpaceDefender/space_defender.py
+++ scripts_python/SpaceDefender/space_defender.py
@@ -53,14 +53,22 @@ except ImportError:
 	# for Python3
 	from tkinter import *
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~ Classes ~~~~~~~~~~~~~~~~~~~~~~~~~
+
 class Partie:
 
 	def __init__(self):
-		self.vitesse=480-4*echelle.get()
-		self.nom_joueur='Anonyme'
+		try:
+			self.difficulty=NouvellePartie.difficulty
+		except Exception:
+			self.difficulty=0
+		# Délai d'actualisation : 480-4*difficulty
+		self.nom_joueur="Anonyme"
 		self.Nom_Joueur()
 		self.pause=1
 		self.perdu=0
+		self.niveau=0
+		music.stop()
 
 	def Nom_Joueur(self):
 		def Joueur(event):
@@ -72,30 +80,42 @@ class Partie:
 			else :
 				# Si le joueur a donné un nom on l'enregistre
 				self.nom_joueur=namefield.get()
+
+			self.difficulty=echelle.get()
+			global PlayerLevel
+			PlayerLevel.config(text="Joueur : {0}\nDifficulté : {1}".format(self.nom_joueur,self.difficulty))
+			woosh.play()
 			Name_Screen.destroy()
 
-				
-		# Création de la fenêtre de sauvegarde
+		# Création de la fenêtre de nouvelle partie
 		Name_Screen = Toplevel()
-		Name_Screen.title('Nom du Joueur')
-		BlablaNom = Label(Name_Screen, text="Nom du joueur pour les scores :").grid()
-		global namefield
-		namefield= Entry(Name_Screen, textvariable="{0}".format(self.nom_joueur))
+		Name_Screen.title('Nouvelle partie')
+
+		BlablaNom = Label(Name_Screen, text="Nom du joueur :").grid(row=0, column=0, padx=6)
+		namefield= Entry(Name_Screen, width=12, textvariable="{0}".format(self.nom_joueur))
 		namefield.focus_force()
-		namefield.grid()
+		namefield.grid(row=1, column=0, padx=6)
 		
+		# Définition de la difficulté
+		echelle = Scale(Name_Screen,from_=0,to=10,resolution=1,orient=HORIZONTAL,\
+		length=100,width=15, label="Difficulté :")
+		echelle.set(self.difficulty)
+		# On affiche l'echelle de difficulté
+		echelle.grid(row=0, rowspan=2, column=1, padx=6)	
+
 		# Bouton OK
 		BoutonOk=Button(Name_Screen, text ='Ok')
 		BoutonOk.bind("<Button-1>", Joueur)
 		Name_Screen.bind("<Return>", Joueur)
-		BoutonOk.grid()
-
+		BoutonOk.grid(row=2, columnspan=2)
 
 class Vaisseau():
 
 	def __init__(self):
 		# Position du vaisseau, toujours dans le premier plan du cube ([etage, colonne])
 		self.position=[4,3]
+		self.bonus_available=0
+		self.bonus_count=0
 
 	def move(self,direction):	
 		if (direction == 'Up'):
@@ -107,23 +127,56 @@ class Vaisseau():
 		elif (direction == 'Right'):
 			self.position[1]+=1
 
+	def fire(self):
+		# Si des tirs bonus sont disponibles
+		if (self.bonus_count>0):
+			laser_fat.play()
+			# On enlève un tir bonus
+			self.bonus_count-=1	
+		# Si on n'a pas de tir bonus en stock	
+		else:
+			laser.play()
+
+	def use_bonus(self):
+		# On remonte le nombre de tirs bonus disponibles
+		self.bonus_count=10
+		# Le bonus est utilisé, il n'est plus disponible
+		self.bonus_available=0
+
 class Attaquant():
 	def __init__(self):
 		# Position initiale de l'attaquant dans le dernier plan du cube ([etage, ligne, colonne])
 		self.position=[4,0,3]
 
+# ~~~~~~~~~~~~~~~~~~~~~~~ Fonctions ~~~~~~~~~~~~~~~~~~~~~~~~
 
+# Un appui sur une touche appelle cette fonction gérant l'interactivité
 def Touche(event):
 	# Si on presse Escape on quitte la fenêtre
 	if (event.keysym=='Escape'):
 		Mafenetre.destroy()
 
-	# Les touches directionnelles font bouger le vaisseau dans les limites du cube	
-	if	((event.keysym=='Up' and NouveauVaisseau.position[0]>0) or 
-		(event.keysym=='Down' and NouveauVaisseau.position[0]<7) or\
-		(event.keysym=='Left' and NouveauVaisseau.position[1]>0) or\
-		(event.keysym=='Right' and NouveauVaisseau.position[1]<7)):
-			NouveauVaisseau.move(event.keysym)
+	# On n'effectue des actions de jeu qu'en dehors du mode pause	
+	if (NouvellePartie.pause==0):
+		
+		# Les touches directionnelles font bouger le vaisseau dans les limites du cube	
+		if	((event.keysym=='Up' and NouveauVaisseau.position[0]>0) or 
+			(event.keysym=='Down' and NouveauVaisseau.position[0]<7) or\
+			(event.keysym=='Left' and NouveauVaisseau.position[1]>0) or\
+			(event.keysym=='Right' and NouveauVaisseau.position[1]<7)):
+				NouveauVaisseau.move(event.keysym)
+
+		# Touche de tir	
+		if 	(event.keysym=='f'):
+			NouveauVaisseau.fire()
+
+		# Si un bonus est disponible, on l'active avec la touche "q"	
+		if 	(event.keysym=='q' and NouveauVaisseau.bonus_available!=0):
+			NouveauVaisseau.use_bonus()
+
+		# Touche de cheat pour donner des bonus	
+		if 	(event.keysym=='c'):
+			NouveauVaisseau.bonus_available+=1	
 
 	# Si on presse espace et qu'on n'a pas perdu on se met en pause ou on en sort
 	if (event.keysym=='space' and NouvellePartie.perdu == 0):
@@ -141,6 +194,7 @@ mixer.init(44100)
 try:
     # Create the sound instances
     music = mixer.Sound("Sound/E1M1.wav")
+    woosh = mixer.Sound("Sound/woosh.wav")
     laser = mixer.Sound("Sound/laser.wav") 
     laser_fat = mixer.Sound("Sound/laser_fat.wav")
 
@@ -152,26 +206,23 @@ except:
 Mafenetre = Tk()
 Mafenetre.title('Space Defender')
 
-
-# Un appui sur le clavier appelle la fonction Touche() qui actualisera la direction
+# Un appui sur le clavier appelle la fonction Touche()
 Mafenetre.bind('<Key>', Touche)
 
-# Création d'un widget Scale pour la gestion de la difficulté
-echelle = Scale(Mafenetre,from_=0,to=10,resolution=1,orient=HORIZONTAL,\
-length=300,width=20,label="Difficulté")
-# On affiche l'echelle de difficulté
-echelle.grid(row=2,  column=0)
+# Indication du joueur et du niveau en cours
+PlayerLevel = Label(Mafenetre, text="Créer une nouvelle partie :", justify=LEFT) 
+PlayerLevel.grid(padx=30, pady=5)
 
-NouveauVaisseau=Vaisseau()
-
-# Lancer une nouvelle partie
 def NouvPart(event):
+	woosh.play()
 	global NouvellePartie
 	NouvellePartie=Partie()
-BoutonNouvPart= Button(Mafenetre, text = "Nouvelle Partie")
+	global NouveauVaisseau
+	NouveauVaisseau=Vaisseau()
+BoutonNouvPart = Button(Mafenetre, text = "Nouvelle Partie")
 BoutonNouvPart.bind("<Button-1>", NouvPart)
 Mafenetre.bind("<Return>", NouvPart)
-BoutonNouvPart.grid()
+BoutonNouvPart.grid(padx=30, pady=5)
 
 
 Mafenetre.mainloop()
--- scripts_python/home.py
+++ scripts_python/home.py
@@ -45,10 +45,18 @@ def Open_snake():
 	os.chdir('Snake3D/')
 	os.system('python snake.py')
 
-# Création de la fenêtre principale
+def Open_space_defender():
+	Mafenetre.destroy()		
+	os.chdir('SpaceDefender/')
+	os.system('python space_defender.py')
+
+# ~~~~~~~~~~~ Création de la fenêtre principale ~~~~~~~~~~~~~~
+
 Mafenetre = Tk()
 Mafenetre.title('Home page')
-
+# Petite indication de la marche à suivre
+SelectHint = Label(Mafenetre, text="Sélectionner le programme à exécuter :", anchor=NW , justify=LEFT)
+SelectHint.grid(row=0, columnspan=3, padx = 5)
 
 # ~~~~~~~~~~~~~~~~~~~ Boutons de lancement ~~~~~~~~~~~~~~~~~~~
 
@@ -56,6 +64,8 @@ Mafenetre.title('Home page')
 Button(Mafenetre, text ='Interface de commande', command = Open_display_commands).grid(row=1, column=0, padx=5, pady=5)
 # Snake3D
 Button(Mafenetre, text ='Snake 3D', command = Open_snake).grid(row=1, column=1, padx=5, pady=5)
+# Space Defender
+Button(Mafenetre, text ='Space Defender', command = Open_space_defender).grid(row=1, column=2, padx=5, pady=5)
 
 # Un appui sur le clavier appelle la fonction Touche()
 Mafenetre.bind('<Key>', Touche)
